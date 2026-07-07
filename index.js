8e856521d39e464d2dad3e29583d049493f735ea
suhaibdev <khalidsuhaib4@gmail.com>
2026-07-08 00:35:08 +0530

    Second Commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 // Load environment variables from .env file
 dotenv.config();
@@ -17,6 +18,16 @@ app.get('/', (req, res) => {
   res.send('Backend server is running!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB');
+  app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+  });
+}).catch((err) => {
+  console.error('Failed to connect to MongoDB', err);
+  process.exit(1);
 });
